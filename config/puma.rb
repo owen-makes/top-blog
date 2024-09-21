cba4ba8059ce165d0e795d2cd796cd0ec4d1f089
owen-makes <owen.botto@gmail.com>
2024-09-21 20:04:12 -0300

    More render config

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,7 @@ plugin :tmp_restart
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+
+preload_app!
