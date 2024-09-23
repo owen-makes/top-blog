fb8c962d75eb73bc8dba803a7ef0e1580539515a
owen-makes <owen.botto@gmail.com>
2024-09-23 11:29:31 -0300

    Remove wait period from build, add predeploy to render.yaml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,13 +6,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# Add this line to wait for the database
-while ! pg_isready -h $RENDER_POSTGRES_HOST -p $RENDER_POSTGRES_PORT -q -U $RENDER_POSTGRES_USER; do
-  echo "Waiting for database..."
-  sleep 2
-done
-
-
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
